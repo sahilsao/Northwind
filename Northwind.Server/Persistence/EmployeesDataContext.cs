8804ee4e23fb6a863766308f3b42af7f49795566
sahilsao <sahilsaocs@gmail.com>
2025-03-03 17:08:27 +0530

    fix

--- Northwind.Server/Persistence/EmployeesDataContext.cs
+++ Northwind.Server/Persistence/EmployeesDataContext.cs
@@ -5,7 +5,7 @@ namespace Northwind.Server.Persistence;
 
 public class EmployeesDataContext(DbContextOptions<EmployeesDataContext> options) : DbContext(options)
 {
-    public DbSet<Employees> Employees { get; set; } 
+    public DbSet<Employees> Employees { get; set; }
 }
 
     
