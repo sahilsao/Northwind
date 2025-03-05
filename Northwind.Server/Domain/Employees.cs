3708815cd257725585f02015f665c3eb2f70778a
sahilsao <sahilsaocs@gmail.com>
2025-03-05 15:47:09 +0530

    fix

--- Northwind.Server/Domain/Employees.cs
+++ Northwind.Server/Domain/Employees.cs
@@ -3,7 +3,7 @@ namespace Northwind.Server.Domain;
 public class Employees
 {
     public int EmployeeId { get; set; }
-    public string LastName { get; set; } = null!;
     public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
     public string Title { get; set; } = null!;
 }
--- Northwind.Shared/Employees/EmployeesDto.cs
+++ Northwind.Shared/Employees/EmployeesDto.cs
@@ -5,7 +5,7 @@ namespace Northwind.Shared.Employees;
 public class EmployeesDto
 {
     public int EmployeeId { get; set; }
-    public string LastName { get; set; } = null!;
     public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
     public string Title { get; set; } = null!;
 }
